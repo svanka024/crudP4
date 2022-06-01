3207384e481f433a5901ca57f346b75120a9921b
jaspervanuden <jaspervanuden03@gmail.com>
2022-06-01 15:35:52 +0200

    login

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php
+include_once "connection.php";
+if(isset($_SESSION['username'])){?>
+   <p><?php echo "welcome " . $_SESSION['username']; ?> <a href="logout.php"> uitloggen</a></p>
+   <?php
+}  else {
+    header("location: login.php");
+} 
+$sql = "SELECT * FROM flights";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+//  WHERE TYPE = "male" (voor welke tabel)
+//$stmt->debugDumpParams();
+
+$result = $stmt->fetchAll();
+?>
+
+<h1>hoi</h1>
