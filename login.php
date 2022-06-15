44b189b64dcd51fd1f5f3fe9cea7f21f3ca15570
svanka024 <1196688@student.roc-nijmegen.nl>
2022-06-15 14:27:40 +0200

    youri

---
+++ login.php
@@ -0,0 +1,106 @@
+<?php 
+include_once "connection.php";
+
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $sql = "SELECT * FROM admin WHERE username = :username AND password = :password";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":username", $_POST['username']);
+    $stmt->bindParam(":password", $_POST['password']);
+    $stmt->execute();
+
+    $result = $stmt->fetchAll();
+
+    if(count($result) > 0){
+        $_SESSION["username"] = $_POST['username'];
+        header('Location: admin.php');
+    exit();
+        
+    } else {
+        echo "gebruikersnaam en of wachtwoord onjuist";
+    }
+} 
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>fly away</title>
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat:400,700"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link type="text/css" rel="stylesheet" href="css/main.css" />
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-white super_float">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="img/logo.png" alt="" width="160" height="160">
+          </a>
+        <a class="navbar-brand text-info" href="index.html">home</a>
+        <button class="navbar-toggler" ty pe="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand text-info" href="information.html">information</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand text-info" href="aboutus.html">about us</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand text-info" href="contact.html">contact</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand text-info" href="login.html"><strong>login</strong></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <div id="booking" class="sectionlogin">
+    <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div class="card bg-white text-info" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+      
+                  <div class="mb-md-5 mt-md-4 pb-5">
+      
+                    <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p class="text-info-50 mb-5">Please enter your login and password!</p>
+                    <form action="login.php" method="post">
+                    <div class="form-outline form-white mb-4" id="loginForm">
+                      <input type="text" id="usernme" name="username" class="form-control form-control-lg" value=""/>
+                      <label class="form-label" for="typeEmailX">username</label>
+                    </div>
+      
+                    <div class="form-outline form-white mb-4">
+                      <input type="password" id="password" name="password" class="form-control form-control-lg" value=""/>
+                      <label class="form-label" for="typePasswordX">Password</label>
+                    </div>      
+                    <input class="btn btn-primary btn-lg px-5" type="submit" value="login"/>     
+                    </form> 
+                  </div>      
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
